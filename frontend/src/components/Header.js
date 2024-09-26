f90c78901a3ec964325557f9f8186701d8a325c8
sumith prabhu <sumithprabhu07@gmail.com>
2024-09-27 00:11:08 +0530

    docs page added

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,43 +1,74 @@
-import React from "react";
-import Image from "next/image"; // Import the Image component
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
 import { usePathname } from "next/navigation";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-import Link from "next/link"; // Import Link from Next.js
+import Link from "next/link";
 
 export default function Header() {
   const pathname = usePathname();
   const isRootPage = pathname === "/";
+  const [isScrollingUp, setIsScrollingUp] = useState(false);
+  const [lastScrollPosition, setLastScrollPosition] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPosition = window.scrollY;
+
+      if (currentScrollPosition > lastScrollPosition) {
+        setIsScrollingUp(false); // Hide navbar when scrolling down
+      } else {
+        setIsScrollingUp(true); // Show navbar when scrolling up
+      }
+      setLastScrollPosition(currentScrollPosition);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollPosition]);
 
   return (
-    <header className="flex justify-between items-center px-6 py-4 bg-black border-b-2 border-neon-green relative z-10">
-      <Link href="/" passHref>
-        <div className="flex items-center">
-          <Image
-            src="/logo.png" // Path to your logo file in the public directory
-            alt="Blitz Protocol Logo"
-            width={50} // Set your desired width
-            height={50} // Set your desired height
-            className="mr-4" // Add some margin to the right
-          />
-          <span className="text-white text-xl font-bold">Blitz Protocol</span>
+    <header
+  className={`${
+    isScrollingUp || lastScrollPosition === 0 ? "opacity-100" : "opacity-0"
+  } fixed top-7 left-0 right-0 mx-auto px-8 py-4 bg-black bg-opacity-70 rounded-full z-50 transition-opacity duration-500 ease-in-out border-2 border-[#FF8B00]`}
+  style={{ maxWidth: "90%", margin: "0 auto" }} // Center the header
+>
+      <div className="flex justify-between items-center w-full">
+        <Link href="/" passHref>
+          <div className="flex items-center cursor-pointer">
+            <Image
+              src="/logo.png" // Path to your logo file in the public directory
+              alt="Blitz Protocol Logo"
+              width={35}
+              height={40}
+              className="mr-4"
+            />
+            <span className="text-white text-2xl font-bold">Blitz Protocol</span>
+          </div>
+        </Link>
+
+        <div className="relative">
+          {isRootPage ? (
+            <a
+              href="/playground"
+              className="text-lg inline-block text-white font-semibold px-6 py-2 rounded-lg border-2 border-[#39FF14] hover:bg-[#39FF14] hover:text-black transition-all duration-300" // Neon border and hover effect
+              style={{ maxWidth: "250px" }}
+            >
+              Launch Playground
+            </a>
+          ) : (
+            <ConnectButton
+              chainStatus="none"
+              label="Connect wallet"
+              accountStatus="address"
+              showBalance={false}
+              className="px-6 py-2 rounded-lg border-2 border-[#39FF14] hover:bg-[#39FF14] hover:text-black transition-all duration-300"
+            />
+          )}
         </div>
-      </Link>
-      <div className="relative">
-        {isRootPage ? (
-          <a
-            href="/playground"
-            className="launch-button text-lg relative inline-block text-white font-semibold px-6 py-2 rounded-lg"
-          >
-            Launch Playground
-          </a>
-        ) : (
-          <ConnectButton
-            chainStatus="none"
-            label="Connect wallet"
-            accountStatus="address"
-            showBalance={false}
-          />
-        )}
       </div>
     </header>
   );
