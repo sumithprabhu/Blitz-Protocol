f1cea85ec3c5701992664cb423e42d6dd2c5ed5a
Sumith prabhu <91826109+sumithprabhu@users.noreply.github.com>
2024-10-04 10:56:08 +0530

    limit increased for payload

--- backend/src/app/index.js
+++ backend/src/app/index.js
@@ -17,7 +17,9 @@ const port = process.env.PORT || 3000;
 
 app.use(cors({origin:true,credentials: true}));
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 
 connectDB();
 
